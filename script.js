aa2b48b1bbf7ebd4c06a874baa0d89d403c428d6
KAMAbee <kamil.akbarov.94@gmail.com>
2024-05-18 13:58:52 +0500

    first

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('card').addEventListener('dblclick', function(){
+    document.querySelector('#front').classList.toggle('flip');
+    document.querySelector('#back').classList.toggle('flip');
+});
+
+
